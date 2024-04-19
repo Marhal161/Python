9f8d679f658bcf98454f95e4cebd752b3770ec0e
John Doe <MaRhAl_161@bk.ru>
2024-04-19 11:04:59 +0300

    cmpl

--- test_bruh/calc_tests.py
+++ test_bruh/calc_tests.py
@@ -1,10 +1,13 @@
 import unittest
 import calc
+import sys
  
 class CalcBasicTests(unittest.TestCase):
+     
     def test_add(self):
         self.assertEqual(calc.add(1, 2), 3)
-        
+
+    @unittest.skipIf(sys.version_info < (3, 6),"Temporaly skip test_add") 
     def test_sub(self):
         self.assertEqual(calc.sub(4, 2), 2)
         
@@ -16,8 +19,9 @@ class CalcBasicTests(unittest.TestCase):
 
 
 class CalcExTests(unittest.TestCase):
+    
     def test_sqrt(self):
         self.assertEqual(calc.sqrt(4), 2)
-        
+           
     def test_pow(self):
         self.assertEqual(calc.pow(3, 3), 27)
